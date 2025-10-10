512216a312151a423a3c512d21c30f15b0b767af
Cohe-rent <akshram.pandiyan@coherent.in>
2025-10-10 16:17:20 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,12 @@
-ECHO is on.
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
+
+output "aks_kube_config" {
+  value = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
